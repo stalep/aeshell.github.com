df44a4bac452c8cb2c63873438c585b589f82f63
Ståle W. Pedersen <stalep@gmail.com>
2012-08-22 15:30:58 +0200

    added a lot more docs on jreadline and gnu readline features

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,8 @@
 require 'docs'
 
 Awestruct::Extensions::Pipeline.new do
-  extension Awestruct::Extensions::Posts.new( '/news' ) 
+  extension Awestruct::Extensions::Posts.new( '/news', :posts ) 
+  extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Docs::Index.new( '/docs/base', :docs_base)
   # extension Awestruct::Extensions::Indexifier.new
 end
